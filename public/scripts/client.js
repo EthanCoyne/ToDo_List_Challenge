e5fd2446855e90f02870fa6eac6a8d95c7622e3f
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-29 12:55:54 -0600

    DELETE functionality working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,7 @@ $(function() {
   getTasks();
   $('#addTaskButton').on('click', addTask);
   $('.displayContainer').on('click', '.complete',  completeTask);
+  $('.displayContainer').on('click', '.delete',  deleteTask);
 
 }); // end doc ready
 
@@ -42,8 +43,8 @@ function displayTasks(tasks) {
     var $li = $('<li></li>');
 
     $li.append('<p id="task'+task.id+'">' + task.task + '</p>');
-    $li.append('<button id="complete'+task.id+'" class="complete">Complete</button>');
-    $li.append('<button id="delete'+task.id+'" class="delete">Delete</button>');
+    $li.append('<button id="'+task.id+'" class="complete">Complete</button>');
+    $li.append('<button id="'+task.id+'" class="delete">Delete</button>');
 
     $('#taskDisplay').append($li);
   }); // end tasks.forEach
@@ -53,3 +54,18 @@ function completeTask() {
   console.log('completeTask called on', $(this).closest('li'));
   $(this).closest('li').addClass('completed');
 }// end completeTask()
+
+function deleteTask() {
+  console.log('deleteTask called on ', $(this).closest('li'));
+  event.preventDefault();
+  var taskId=$(this).attr('id');
+  // var formData = $(this).closest('li').serialize();
+  // console.log('formData: ', formData, 'id: ', taskId);
+  $.ajax({
+  url:'/tasks/'+ taskId,
+  type: 'DELETE',
+  success: function() {
+    getTasks();
+  }
+  }); // end ajax DELETE
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -29,7 +29,7 @@ router.post('/',function(req,res){
       }); // end client.query
     }
   }); // end pool.connect
-});//end of post
+});//end of POST
 
 router.get('/',function(req,res){
   pool.connect(function(err,client,done){
@@ -52,7 +52,33 @@ router.get('/',function(req,res){
       });
     }
   });
-});//end of get
+});//end of GET
+
+router.delete('/:pid',function(req,res){
+  console.log('req.body:',req.body);
+  pool.connect(function(err,client,done){
+    if(err){
+      console.log('error connecting to DB',err);
+      res.sendStatus(500);
+      done();
+    } else {
+
+     client.query(
+       'DELETE FROM tasks WHERE id = $1',
+                    [req.params.pid],
+      function(err,result){
+        done();
+        if(err){
+          console.log('error querying db',err);
+          res.sendStatus(500);
+        } else {
+          console.log('posted info from db',result.rows);
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});//end of DELETE
 
 
 
